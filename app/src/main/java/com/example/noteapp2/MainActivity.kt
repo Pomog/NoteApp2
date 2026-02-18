4dc08109ce7ee7ae082db3d1ac084a448f699297
Yurii Panasiuk <thoryur@gmail.com>
2026-02-18 23:48:04 +0200

    Initial commit: NoteApp2

---
+++ app/src/main/java/com/example/noteapp2/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.noteapp2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.noteapp2.screen.NoteScreen
+import com.example.noteapp2.ui.theme.NoteApp2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NoteApp2Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    NoteScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NoteApp2Theme {
+        NoteScreen()
+    }
+}
