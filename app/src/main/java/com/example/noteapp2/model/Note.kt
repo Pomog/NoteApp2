e62ee30a7ed6c50a58636635b3ed0d61a018dfd0
Pomog <thoryur@gmail.com>
2026-03-04 20:57:50 +0200

    first Entity class ROOM

--- app/src/main/java/com/example/noteapp2/model/Note.kt
+++ app/src/main/java/com/example/noteapp2/model/Note.kt
@@ -2,13 +2,23 @@ package com.example.noteapp2.model
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.Instant
 import java.time.LocalDateTime
+import java.util.Date
 import java.util.UUID
 
 @RequiresApi(Build.VERSION_CODES.O)
+@Entity(tableName = "notes_tbl")
 data class Note(
+    @PrimaryKey
     val id: UUID = UUID.randomUUID(),
+    @ColumnInfo(name = "note_title")
     val title: String,
+    @ColumnInfo(name = "note_description")
     val description: String,
-    val entryDate: LocalDateTime = LocalDateTime.now()
+    @ColumnInfo(name = "note_date")
+    val entryDate: Date = Date.from(Instant.now())
 )
